aceeb442650d3b6ce700bc41869e064e22ca1dff
Mircea <hello@cattlepi.com>
2018-10-25 19:18:31 -0700

    enhance the dirty deploy

--- buildonpi/dirty_deploy.sh
+++ buildonpi/dirty_deploy.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
+# script used to iterate faster on the builder (deploy and run on the fly)
+set -x
 export SELFDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TARGET="192.168.1.87"
-SCRIPT="schedule_work.sh"
-for CSP in $(ls -1 ${SELFDIR})
-do
-    scp $CSP pi@${TARGET}:/sd/
-    sshi pi@${TARGET} chmod +x /sd/$CSP
-done
+
+tar czvf /tmp/builder.tar.gz -C $SELFDIR .
+scp /tmp/builder.tar.gz pi@${TARGET}:/tmp/
+sshi pi@${TARGET} tar -xzvf /tmp/builder.tar.gz -C /sd
+sshi pi@${TARGET} chmod +x /sd/*.sh
