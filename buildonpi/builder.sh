e1e9638af0a94bb934ca310c6f6e3373747910ed
Mircea <hello@cattlepi.com>
2018-10-31 07:37:50 -0700

    checkpoint on build complete

--- buildonpi/builder.sh
+++ buildonpi/builder.sh
@@ -3,6 +3,10 @@ export SELFDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source ${SELFDIR}/functions.sh > /dev/null 2>&1
 BUILDER=$1
 BUILDLOCATION=$2
+load_builder_state $BUILDER
+update_current_time
+BUILDER_LAST_ACTION=${CURRENT_TIME}
+persist_builder_state $BUILDER
 echo "running with builder ${BUILDER} in ${BUILDLOCATION}"
 export BUILDER_NODE=${BUILDER}
 cd ${BUILDLOCATION} && git clone https://github.com/cattlepi/cattlepi.git
@@ -10,6 +14,10 @@ cd ${BUILDLOCATION}/cattlepi && make envsetup
 # test
 
 cd ${BUILDLOCATION}/cattlepi && make test_noop
+update_current_time
+BUILDER_LAST_ACTION=${CURRENT_TIME}
+persist_builder_state $BUILDER
+
 # actual image
 cd ${BUILDLOCATION}/cattlepi && make raspbian_cattlepi
 BUILDRESULT=$?
@@ -21,4 +29,9 @@ if [ $BUILDRESULT -ne 0 ]; then
 else
     echo "build SUCCESSFUL"
 fi
+
+update_current_time
+BUILDER_LAST_ACTION=${CURRENT_TIME}
+BUILDER_STATE="rebuild"
+persist_builder_state $BUILDER
 echo "-------------------------"
