e3a40aae62db476079ebef52a7c58bf49a642f80
Mircea <hello@cattlepi.com>
2018-11-04 08:04:11 -0800

    update user (no longer hardcoded) + update TODO

--- buildonpi/setup_02configs.sh
+++ buildonpi/setup_02configs.sh
@@ -2,5 +2,5 @@
 export RASPBIAN_LOCATION=http://downloads.raspberrypi.org/raspbian_lite/images/raspbian_lite-2018-06-29/2018-06-27-raspbian-stretch-lite.zip
 export RASPBIAN_FILE=2018-06-27-raspbian-stretch-lite.zip
 
-export GITHUB_API_USER="mirceal"
+export GITHUB_API_USER=$(jq -r ".config.buildcontrol.gh_user" /tmp/current_config)
 export GITHUB_API_TOKEN=$(jq -r ".config.buildcontrol.gh_token" /tmp/current_config)
