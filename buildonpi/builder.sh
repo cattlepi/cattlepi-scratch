3d8799a1ecf5fdd5420ddf3598b607a80609e0c1
Mircea <hello@cattlepi.com>
2018-11-08 21:15:05 -0800

    set path

--- buildonpi/builder.sh
+++ buildonpi/builder.sh
@@ -2,6 +2,7 @@
 export SELFDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source ${SELFDIR}/functions.sh > /dev/null 2>&1
 source $WORKDIR/cattlepi/tools/venv/bin/activate > /dev/null 2>&1
+export PATH=${PATH}:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
 BUILDER=$1
 BUILDLOCATION=$2
