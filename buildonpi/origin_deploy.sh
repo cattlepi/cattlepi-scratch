10fd41c0ccf13c28fc2c57b285249a69d168a247
Mircea <hello@cattlepi.com>
2018-11-10 20:14:51 -0800

    update deploy hash

--- buildonpi/origin_deploy.sh
+++ buildonpi/origin_deploy.sh
@@ -4,6 +4,6 @@ set -x
 mkdir -p /var/tmp/clone
 cd /var/tmp/clone && git clone https://github.com/cattlepi/cattlepi-scratch.git
 cd /var/tmp/clone/cattlepi-scratch && git fetch origin +refs/pull/*/merge:refs/remotes/origin/pr/*
-cd /var/tmp/clone/cattlepi-scratch && git reset --hard 97d0f45cc9487173747f24b9e332b78681bca6cb
+cd /var/tmp/clone/cattlepi-scratch && git reset --hard 78f40278ad816281c54ab8995a22ed9220c4583d
 sudo /var/tmp/clone/cattlepi-scratch/buildonpi/setup.sh
 sudo /var/tmp/clone/cattlepi-scratch/buildonpi/install_monitor.sh
