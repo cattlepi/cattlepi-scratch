65eb268dc6196e1ba1d093a18ec5d48d32257b2f
Mircea <hello@cattlepi.com>
2018-11-23 11:48:01 -0800

    pick up the lastest from origin/master if a commit hash is not passed in as a parameter in origin_deploy

--- buildonpi/origin_deploy.sh
+++ buildonpi/origin_deploy.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 # script used to deploy the builder on a pi via the boot script
 set -x
+
+COMMIT_HASH="origin/master"
+if [ "$#" -eq 1 ]; then
+    COMMIT_HASH=$1
+fi
+
 mkdir -p /var/tmp/clone
 cd /var/tmp/clone && git clone https://github.com/cattlepi/cattlepi-scratch.git
 cd /var/tmp/clone/cattlepi-scratch && git fetch origin +refs/pull/*/merge:refs/remotes/origin/pr/*
-cd /var/tmp/clone/cattlepi-scratch && git reset --hard 28370a5e79e9e10357c4e3c8afdad100574af3c5
+cd /var/tmp/clone/cattlepi-scratch && git reset --hard ${COMMIT_HASH}
 sudo /var/tmp/clone/cattlepi-scratch/buildonpi/setup.sh
 sudo /var/tmp/clone/cattlepi-scratch/buildonpi/install_monitor.sh
