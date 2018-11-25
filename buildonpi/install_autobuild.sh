6d23d4606286997f99c1222bd3f366c69ab310f5
Mircea <hello@cattlepi.com>
2018-11-25 15:15:19 -0800

    clean builder state on rebuild + autobuild every 6 hours

--- buildonpi/install_autobuild.sh
+++ buildonpi/install_autobuild.sh
@@ -2,4 +2,4 @@
 export SELFDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source ${SELFDIR}/functions.sh > /dev/null 2>&1
 
-echo "0 */3 * * *   pi    touch ${AUTOBUILDREQUESTED}" > /etc/cron.d/cattlepi_autobuild
+echo "0 */6 * * *   pi    touch ${AUTOBUILDREQUESTED}" > /etc/cron.d/cattlepi_autobuild
